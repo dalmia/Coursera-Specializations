8c7d261bf6ec617c90b0b5ef66e1fd0fe10f313a
Aman Dalmia <amandalmia18@gmail.com>
2016-10-29 09:22:20 +0530

    Completing Assignment 1 Week 4

---
+++ Specializations/Python Specialization/Course 3/urllib1.py
@@ -0,0 +1,21 @@
+# Note - this code must run in Python 2.x and you must download
+# http://www.pythonlearn.com/code/BeautifulSoup.py
+# Into the same folder as this program
+
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter - ')
+html = urllib.urlopen(url).read()
+
+soup = BeautifulSoup(html)
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+sumV=0
+
+for tag in tags:
+    # Look at the parts of a tag
+    sumV+=int(tag.contents[0])
+
+print sumV
